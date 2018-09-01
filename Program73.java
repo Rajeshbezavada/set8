e9c0bb516f69014869cd5462dabae33a8e6d4ae7
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-09-01 00:23:27 -0400

    Create Program73.java

---
+++ Program73.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Program73
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int l=sc.nextInt();
+		int r=sc.nextInt();
+		if(n>l&&n<r)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
