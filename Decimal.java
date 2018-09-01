5a53e8ea45fadee2d6ede00ca5b4b3556ceec266
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-09-01 00:31:39 -0400

    Create Decimal.java

---
+++ Decimal.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Decimal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		float f=sc.nextFloat();
+		int r=(int)Math.ceil(f);
+		System.out.println(r);
+	}
+}
